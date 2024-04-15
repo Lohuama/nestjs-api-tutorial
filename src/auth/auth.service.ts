1cd9a1560af8ddbfcbba597779afa1af924ded7a
Lohuama <limalorruama98@gmail.com>
2024-04-15 17:06:53 -0300

    hash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-
+import * as argon from 'argon2'
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
+  async signup(dto: AuthDto) {
+    // gerar a senha com hash
+     const hash = argon.hash(dto.password)
+
+    // salvar new user no banco
+
+    //retornar novo usuario
+
     return { msg: 'signup' };
   }
   signin() {
