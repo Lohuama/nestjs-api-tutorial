5d80f81dfea547015eef9e6f2a46fe897fba1d53
Lohuama <limalorruama98@gmail.com>
2024-04-29 09:12:54 -0300

    first commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controllers";
 import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
+import { JwTStrategy } from "src/strategy/jwt.strategy";
 
 @Module({
     imports: [JwtModule.register({})],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, JwTStrategy]
 })
 export class AuthModule{}
